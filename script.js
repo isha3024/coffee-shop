e7904aa0f0a89870529a6f01138c2aba40524365
isha3024 <gandhiisha45516@gmail.com>
2023-10-11 14:41:45 +0530

    Dynamic Code of Banner Slider

--- script.js
+++ script.js
@@ -11,6 +11,43 @@ var swiper = new Swiper(".slider", {
     }
 });
 
+const bannerSliderObj = [
+  {
+    "id": 1,
+    "image": "img/homeImg1.jpg",
+    "subtitle": "We really like what we do.",
+    "title": "Coffee Beans with a <br>Perfect Aroma"
+  },
+  {
+    "id": 2,
+    "image": "img/homeImg2.jpg",
+    "subtitle": "Enjoy the finest coffee drinks.",
+    "title": "Enjoy our exclusive <br>Coffee and Cocktails"
+  },
+  {
+    "id": 3,
+    "image": "img/homeImg3.jpg",
+    "subtitle": "Making our coffee with love.",
+    "title": "Alluring and Fragrant <br>Coffee Aroma"
+  }
+]
+
+for(let i = 0; i< bannerSliderObj.length; i++){
+  const bannerSlider = document.getElementById('bannerSlider');
+  const child = document.createElement('div');
+  child.classList.add('sliderItem', 'swiper-slide');
+  child.innerHTML = 
+  `<img src="${bannerSliderObj[i].image}" alt="Home image-${bannerSliderObj[i].id}" class="home-img">
+  <div class="home-details">
+      <div class="home-text">
+          <h4 class="homeSubtitle">${bannerSliderObj[i].subtitle}</h4>
+          <h2 class="homeTitle">${bannerSliderObj[i].title}</h2>
+      </div>
+      <button class="button">Explore</button>
+  </div>`;
+  bannerSlider.appendChild(child);
+}
+
 const main = document.querySelector('.main');
 const header = document.getElementById("header");
 const sections = document.querySelectorAll('.section-main');
